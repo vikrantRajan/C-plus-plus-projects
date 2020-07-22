a056a1b951b157308ca04caa1c4c058fd59def3c
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-07-22 00:19:08 -0700

    Multidimensional Arrays

--- Basics/11_arrays.cpp
+++ Basics/11_arrays.cpp
@@ -4,15 +4,18 @@ using namespace std;
 
 int main() 
 {
-    int array[3]; // VARIABLE TYPE [SIZE OF ARRAY]
+    int multiArray[3][4] = {0}; // VARIABLE TYPE [NUMBER OF ARRAYS] [NUMBER OF ELEMENTS FOR EACH ARRAY]
 
-    array[0] = 10;
-    array[1] = 14;
-    array[2] = 15;
+    cout << multiArray[0][0] << endl;
+    cout << multiArray[0][1] << endl;
+    cout << multiArray[0][2] << endl;
+    cout << multiArray[0][3] << endl;
 
-    cout << "array [0] = " << array[0] << ", address: " << &array[0] << endl;
-    cout << "array [1] = " << array[1] << ", address: " << &array[1] << endl;
-    cout << "array [2] = " << array[2] << ", address: " << &array[2] << endl;
+    cout << &multiArray[0][0] << endl;
+    cout << &multiArray[0][1] << endl;
+    cout << &multiArray[0][2] << endl;
+    cout << &multiArray[0][3] << endl;
+
 
 
 
