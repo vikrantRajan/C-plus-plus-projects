75c9e907341b0f00ba3e1cdad323b799575d1538
vikrantRajan <vikrantrajan93@gmail.com>
2020-09-19 02:07:36 -0700

    Dynamic Memory Allocation

---
+++ Pointers/dynamicMemoryAllocation.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+/* dynamic allocation of memory */
+
+int main()
+{
+   // int var = 41240;
+  
+    int amount;
+
+    cout << "How many numbers would you like to store in an array? " << endl;
+    cin >> amount;
+
+    int *p = new (nothrow) int[amount];
+
+    if (p != NULL)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            cout << "Enter the " << (i+1) << " number: " << endl;
+            cin >> p[i];
+        }
+
+        for (int i = 0; i < amount; i++)
+        {
+            cout << "p [ " << i << " ] =  " << p[i] << endl;
+        }
+    }
+    else
+        cout << "Not enough memory " << endl;
+       
+
+
+    {
+        int *p = new int; // this reserves the same space of memory
+        cout << p << endl;
+        delete p; // If we dont delete, another space in memory gets allocated for the next line
+        p = new int; // this reserves the same space of memory
+        cout << p << endl;
+        delete p;
+
+
+    }
+
+
+    //cout << "var: " << var << endl;
+
+
+    delete []p;
+    return 0;
+}
